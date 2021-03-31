c2eea8a9a35f81482253cf3ac78b474a9677d241
jooyoung-korea <60770121+jooyoung-korea@users.noreply.github.com>
2021-03-31 20:55:52 +0900

    Update env.py
    made make_walls()

--- environment/env.py
+++ environment/env.py
@@ -1,34 +1,72 @@
 import numpy as np
-
+import random
+import sys
+# np.set_printoptions(threshold=sys.maxsize) #  If you get a truncated representation, but want the full array, try this!
 
 class Map():
 
     ## 에이 전트 위치
-    def __init__(self, height, width, num_walls):
+    def __init__(self, max_height, max_width, max_num_walls):
         """
-
+        init 에선 random한 크기의 empty한 맵 설정 및 파라미터 설정
+        random하게 많이 생성하여 다양한 환경에서의 학습을 시켜본다.
         :param height:
         :param width:
         :param num_walls:
         """
 
-        self.width = width
-        self.height = height
+        self.width = np.random.randint(1,max_width)
+        self.height = np.random.randint(1, max_height)
+        self.num_walls = np.random.randint(1,max_num_walls)
 
-        for i in range(num_walls):
+        # Empty map 생성하기
+        self.map = np.zeros((self.width, self.height))
 
-        wall_len = min(height, width)
-
-        walls = get_walls()
 
     # Make walls
-    def get_walls(self,x, y):  # (x,y) is the starting point
+    def make_walls(self):  # (x,y) is the starting point
+        """
+        :return: map occupied(1) by some walls
         """
 
-        :param x:
-        :param y:
-        :return:
-        """
+
+        for i in range(self.num_walls):
+            # 벽의 시작점
+            x0 = np.random.randint(0,self.width)
+            y0 = np.random.randint(0,self.height)
+
+            # length of the wall -> 2부터 시작하는게 맞는 거 같애 not 0! 흠 장애물처럼 1개짜리도 있을 수 있다고 할까 그럼 그냥 0!
+            w_len = np.random.randint(0,min(self.width-x0, self.height-y0))
+
+            # shape of the wall (horizontal, vertical, 45) - tuple contains a int
+            ang = np.random.choice(3, 1, p=[0.3, 0.3, 0.4])
+
+            if ang[0] == 0:
+                x1 = x0 + w_len
+                y1 = y0
+            elif ang[0] == 1:
+                x1 = x0
+                y1 = y0 + w_len
+            else:
+                x1 = x0 + w_len
+                y1 = y0 + w_len
+
+            xs = np.arange(x0, x1)
+            ys = np.arange(y0, y1)
+
+            # if (x0 == x1) or (y0 == y1) then xs, ys -> []
+            if x0 == x1:
+                xs = [x0]*w_len
+            if y0 == y1:
+                ys = [y0]*w_len
+
+            # If the pixel? is occupied by the wall, let's put 1 there
+            for (x_,y_) in zip(xs, ys):
+                self.map[(x_,y_)] = 1
+
+        # print(self.map)
+
+
     def env_state(self):
         """
         agent가 차지하고 있는 위치
@@ -43,54 +81,6 @@ class Map():
         """
 
 
-
-
-# 시작점
-x0 = np.random.randint(0,width)
-y0 = np.random.randint(0,height)
-print(f'x0: {x0}, y0: {y0}')
-
-# 끝나는 점
-# x1 = np.random.randint(x0, width)
-# y1 = np.random.randint(y0, height)
-# 위와 같은 식으로 하면 중간에 occupied 되는 pixel?이 모호? 일단은 정사각형 형태로 되는걸로!
-
-w_len = np.random.randint(0,min(width-x0, height-y0))
-print(f'w_len: {w_len}')
-
-cho = np.random.choice(3, 1, p=[0.3, 0.3,0.4])
-print(f'choice to make wall: {cho[0]}')
-
-if cho[0] == 0:
-  x1 = x0 + w_len
-  y1 = y0
-
-elif cho[0] == 1:
-  x1 = x0
-  y1 = y0 + w_len
-
-elif cho[0] == 2:
-  x1 = x0 + w_len
-  y1 = y0 + w_len
-
-print(f'(x1,y1): ({x1},{y1})')
-
-# np.where()
-xs = np.arange(x0, x1)
-if x0 == x1:
-  print(type(xs))
-  xs = [x0]*w_len
-
-print(f'xs: {xs}')
-
-
-ys = np.arange(y0, y1)
-if not ys:
-  print(type(ys))
-  ys = [y0]*w_len
-
-print(f'ys: {ys}')
-
-
-
-# 연결하면 벽
+if __name__ == "__main__":
+    test_map = Map(20,20,10)
+    test_map.make_walls() # If you wanna see the map, turn on the print code!
